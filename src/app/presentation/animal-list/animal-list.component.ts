4c498e22e58d91867e895aca22fa948ec56ca6a8
tanasitaint <tanasit.a@gofive.co.th>
2024-05-27 05:38:54 +0700

    destroy subscription

--- src/app/presentation/animal-list/animal-list.component.ts
+++ src/app/presentation/animal-list/animal-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimalModel } from '../../core/models/animal.model';
 import { GetAllAnimalsUseCase } from '../../core/usecases/animal/get-animals.usecase';
+import { ReplaySubject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-animal-list',
@@ -11,15 +12,21 @@ import { GetAllAnimalsUseCase } from '../../core/usecases/animal/get-animals.use
   templateUrl: './animal-list.component.html',
   styleUrl: './animal-list.component.scss'
 })
-export class AnimalListComponent {
+export class AnimalListComponent implements OnInit, OnDestroy {
   constructor(private getAllAnimal: GetAllAnimalsUseCase) { }
 
   animals: AnimalModel[] = [];
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   ngOnInit() {
-    this.getAllAnimal.execute().subscribe((data) => {
+    this.getAllAnimal.execute().pipe(takeUntil(this.destroyed$)).subscribe((data) => {
       this.animals = data;
     });
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 
   onSelectedAnimal(name: string) {
